377c80cf4c4bfcbc578fa58282f51a9503711697
Nils Esper <esper@physik.rwth-aachen.de>
2026-07-19 16:51:30 +0200

    add bash wrapper for skimming step

--- scripts_analysis/skimming/run_skimming_data.sh
+++ scripts_analysis/skimming/run_skimming_data.sh
@@ -15,15 +15,26 @@ ProcId=$5
 # source scripts_analysis/skimming/run_skimming_data.sh scripts_analysis/skimming/test_files/input_large_test_0.root scripts_analysis/skimming/test_files/output_large_test_0_nocuts.root config_analysis/skimming/SkimmingParamsAnalysis_data.yaml
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-CYAN="\033[0;36m"
-WHITE="\033[0;37m"
-WHITEBOLD="\033[1;37m"
-YELLOW="\033[0;33m"
-RED="\033[0;31m"
-GREEN="\033[0;32m"
 RESET="\033[0m"
+BLACK='\033[0;30m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+BGBLACK='\033[0;40m'
+BGRED='\033[0;41m'
+BGGREEN='\033[0;42m'
+BGYELLOW='\033[0;43m'
+BGBLUE='\033[0;44m'
+BGPRUPLE='\033[0;45m'
+BGCYAN='\033[0;46m'
+BGWHITE='\033[0;47m'
 
 CURRENT_WORKING_DIR="`pwd`"
+
 InputFilePath="`realpath -s ${InputFile}`"
 OutputFilePath="`realpath -s ${OutputFile}`"
 ParamsFilePath="`realpath -s ${ParamsFile}`"
@@ -43,14 +54,15 @@ printf "${GREEN}>>>>>> ${WHITE}   ProcId = ${ProcId} ${RESET}\n"
 
 printf "${GREEN}>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>${RESET}\n"
 printf "${GREEN}>>>>>> ${WHITE} SOURCING ENVIRONMENT ${RESET}\n"
+
 cd /home/home1/institut_3a/esper/promotion/test_analysis_hscp_l1/analysis_controller_repository/
 source env_no-voms.sh
 
 printf "${GREEN}>>>>>> ${WHITE} STARTING SCRIPT AND TIMING IT ${RESET}\n"
+
 cd /home/home1/institut_3a/esper/promotion/test_analysis_hscp_l1/analysis_controller_repository/scripts_analysis/skimming/
-/usr/bin/time --verbose  python skimming_data.py --input ${InputFilePath} --output ${OutputFilePath} --params ${ParamsFilePath}
+python skimming_data.py --input ${InputFilePath} --output ${OutputFilePath} --params ${ParamsFilePath}
 
 printf "${GREEN}>>>>>> ${WHITE} DONE ${RESET}\n"
 cd ${CURRENT_WORKING_DIR}
-
 printf "${GREEN}>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>${RESET}\n"
